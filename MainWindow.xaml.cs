56cd443b83d2f005058e57bf841fdc172bbda7a1
Aidan McClung <36234902+MysticalWarrior@users.noreply.github.com>
2018-04-13 18:04:47 -0400

    Create MainWindow.xaml.cs

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace u3Functions
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+            double.TryParse(txtLength.Text, out double length);
+            double.TryParse(txtWidth.Text, out double width);
+            double peri = length + length + width + width;
+            MessageBox.Show("Perimeter is: " + peri.ToString());
+        }
+    }
+}
